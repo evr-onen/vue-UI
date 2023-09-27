452cf87f8b9925b13c7c04a28cebf1fd68651966
evr.onen <evr.onengmail.com>
2023-09-27 09:25:24 +0300

    use field array testing

--- src/views/deneme/parentComp.vue
+++ src/views/deneme/parentComp.vue
@@ -1,41 +1,16 @@
+<script setup>
+import { ref } from "vue";
+import { Form, Field } from "vee-validate";
+const onSubmit = (values) => {
+	console.log(values);
+};
+</script>
 <template>
-	<div class="w-full flex justify-center mt-20">
-		<div class="">
-			<ul class="flex">
-				<childComp @click="deleteItem(index)" v-for="(item, index) in ttt" :key="index" :t="item" />
-			</ul>
-		</div>
+	<div class="">
+		<Form @submit.prevent="onSubmit">
+			<Field name="username" type="text" />
+			<button class="buttonOne">submit</button>
+		</Form>
 	</div>
 </template>
-
-<script setup>
-import { ref, computed } from "vue";
-import childComp from "./childComp.vue";
-// import { products } from "@/constant/products";
-
-// const yyy = ref(products);
-
-// const deleteItem = (index) => {
-// 	yyy.value = yyy.value.filter((_, ind) => ind !== index);
-// };
-</script>
-
-<style>
-li + li {
-	@apply ml-2;
-}
-</style>
-
-<!-- let ttt = computed({
-	get() {
-		return yyy.value;
-	},
-	set(val) {
-		console.log(ttt.value);
-		yyy.value = val;
-	},
-});
-const deleteItem = (index) => {
-	
-	ttt.value = yyy.value.filter((_, ind) => ind !== index);
-}; -->
+<style scoped></style>
