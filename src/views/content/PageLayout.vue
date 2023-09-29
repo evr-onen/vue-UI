76960c43f979fbe765cf39470594313c2a569b28
evr.onen <evr.onengmail.com>
2023-09-29 18:20:55 +0300

    FieldArray in FieldArray usage succes!

--- src/views/content/PageLayout.vue
+++ src/views/content/PageLayout.vue
@@ -1,12 +1,80 @@
+<template>
+	<div class="w-full flex flex-col items-center">
+		<router-link to="/dashboard/welcome" class="buttonOne">Go to Dashboard</router-link>
+		<div class="w-full max-w-[1240px] px-4">
+			<h1>vee-validate array fields</h1>
+
+			<form class="bg-cyan-50 p-4" @submit.prevent="onSubmit">
+				<p></p>
+
+				<FieldArray name="users" v-slot="{ fields, push, remove, update }">
+					<div
+						class="InputGroup relative group/item border-2 border-backGroundPrimary p-4 flex items-center mt-4"
+						v-for="(field, idx) in fields"
+						:key="field.key"
+					>
+						<h2 class="mr-4">User #{{ idx + 1 }}</h2>
+						<FieldArray
+							:name="`users[${idx}].names`"
+							v-slot="{ fields: nameFields, push: namesPush, remove: namesRemove }"
+						>
+							<div v-for="(nameField, ind) in nameFields" :key="ind" class="flex items-center">
+								<div class="mr-4 flex items-center justify-center relative">
+									<p class="uppercase text-sm font-semibold text-backGroundPrimary mr-2">Pet Name {{ ind + 1 }} :</p>
+									<div class="flex flex-col relative">
+										<Field
+											as="input"
+											class="inputOne"
+											v-bind="nameField"
+											type="text"
+											:name="`users[${idx}].names[${ind}]`"
+										/>
+										<ErrorMessage
+											:name="`users[${idx}].names[${ind}]`"
+											class="text-red-600 mt-1 rounded-md text-center capitalize text-xs absolute bottom-full font-semibold"
+										/>
+									</div>
+									<button
+										v-if="nameFields.length === ind + 1 && ind < 2"
+										class="buttonOne rounded-full bg-backGroundPrimary absolute hidden -right-16 group-hover/item:block"
+										type="button"
+										@click="namesPush('')"
+									>
+										+
+									</button>
+								</div>
+							</div>
+
+							<button
+								class="buttonOne rounded-full hidden bg-backGroundPrimary absolute -right-0 group-hover/item:block"
+								type="button"
+								@click="remove(idx)"
+							>
+								Remove User
+							</button>
+						</FieldArray>
+					</div>
+					<button class="buttonOne bg-backGroundPrimary" type="button" @click="push({ names: [''] })">
+						Add User +
+					</button>
+					<button class="buttonOne bg-backGroundPrimary" type="submit">Submit</button>
+				</FieldArray>
+			</form>
+		</div>
+	</div>
+</template>
+
 <script setup>
-import { ref } from "vue";
+import { ref, watchEffect } from "vue";
 import { Form, Field, ErrorMessage, FieldArray, useFieldArray, useForm, configure } from "vee-validate";
 import * as yup from "yup";
+
+const rrr = ref(1);
 
 const initialData = {
 	users: [
 		{
-			name: "",
+			names: [""],
 		},
 	],
 };
@@ -16,69 +84,35 @@ const schema = yup.object().shape({
 		.array()
 		.of(
 			yup.object().shape({
-				name: yup.string().required("it must not be empty"),
+				names: yup.array().of(yup.string().required("bos birakilamaz")),
 			})
 		)
 		.strict(),
 });
+
 const { errors, setErrors, setFieldValue, handleSubmit } = useForm({
 	initialValues: initialData,
 	validationSchema: schema,
 });
 
-const { fields, push, remove } = useFieldArray("users");
+// it is not need but u can use for its methods
+// const { fields: usersFields, push, remove } = useFieldArray("users");
+
+const { fields: namesFields, push: namesPush, remove: namesRemove } = useFieldArray("names");
+
 configure({
 	bails: false,
 	validateOnInput: true,
 });
+
 const onSubmit = handleSubmit((data) => {
 	console.log(JSON.stringify(data));
+	console.log(namesFields.value);
 });
 </script>
 
-<template>
-	<div class="w-full flex flex-col items-center">
-		<router-link to="/dashboard/welcome" class="buttonOne"> go to Dashboard</router-link>
-		<div class="max-w-[700px] w-full">
-			<h1>vee-validate array fields</h1>
-
-			<form class="border-2 border-backGroundPrimary bg-cyan-50 p-4" @submit.prevent="onSubmit">
-				<p></p>
-
-				<FieldArray name="users" v-slot="{ fields, push, remove }">
-					<fieldset class="InputGroup relative group/item" v-for="(field, idx) in fields" :key="field.key">
-						<h2>User #{{ idx + 1 }}</h2>
-						<div class="flex flex-col">
-							<label class="text-backGroundPrimary font-semibold" :for="`name_${idx}`">Name</label>
-							<Field
-								as="input"
-								type="text"
-								v-model="field.value.name"
-								class="px-4 py-2 border border-black focus-visible:outline-cyan-800 focus-visible:rounded-none"
-								:id="`name_${idx}`"
-								:name="`users[${idx}].name`"
-							/>
-							<!-- {{ errorMessage }} -->
-							<ErrorMessage :name="`users[${idx}].name`" />
-						</div>
-						<button
-							class="buttonOne rounded-full bg-backGroundPrimary absolute -top-4 right-0 group-hover/item:block"
-							type="button"
-							@click="remove(idx)"
-						>
-							X
-						</button>
-					</fieldset>
-
-					<button class="buttonOne bg-backGroundPrimary" type="button" @click="push({ name: '' })">Add User +</button>
-				</FieldArray>
-				<button class="buttonOne bg-backGroundPrimary" type="submit">Submit</button>
-			</form>
-		</div>
-		<div class="">
-			{{ errors }}
-		</div>
-	</div>
-</template>
-
-<style></style>
+<style>
+.inputOne {
+	@apply border-2 border-backGroundPrimary h-full pl-2;
+}
+</style>
